4ee50d9be0699890214ca95e03ef3e423ac66374
Martin Harman <martinharman99@outlook.com>
2024-12-02 20:19:04 +0100

    feat: Add 2024-02 part2

--- 2024/day_02.py
+++ 2024/day_02.py
@@ -11,29 +11,55 @@ def read_file():
     return reports
 
 
+def report_checker(report: list):
+    prev_level = None
+    is_asc = False
+    is_desc = False
+    incorrect_idx = []
+    for i, level in enumerate(report):
+        if prev_level is None:
+            prev_level = level
+            continue
+        diff = prev_level - level
+        if not (abs(diff) >= 1 and abs(diff) <= 3):
+            incorrect_idx.append(i)
+        if diff < 0:
+            is_asc = True
+        else:
+            is_desc = True
+        if is_asc and is_desc:
+            incorrect_idx.append(i)
+        prev_level = level
+    return incorrect_idx
+
+
 def part1(reports):
     safe_count = 0
     for report in reports:
-        prev_level = None
-        diffs = []
-        for level in report:
-            if prev_level is None:
-                prev_level = level
-                continue
-            diff = prev_level - level
-            diffs.append(diff)
-            prev_level = level
-        if not all(abs(diff) >= 1 and abs(diff) <= 3 for diff in diffs):
-            continue
-        if report == sorted(report) or report == sorted(report, reverse=True):
-            print(f"{report=} is safe!")
+        result = report_checker(report)
+        if not result:
             safe_count += 1
     print(f"Part 1: result = {safe_count}")
 
 
 def part2(reports):
-    pass
-    # print(f"Part 2: result = {safe_count}")
+    safe_count = 0
+    for report in reports:
+        incorrect_idx = report_checker(report)
+        if not incorrect_idx:
+            safe_count += 1
+        else:
+            # Add fist/last index edge case
+            incorrect_idx.append(1)
+            incorrect_idx.append(len(report))
+            for i in incorrect_idx:
+                report_copy = report.copy()
+                report_copy.pop(i - 1)
+                incorrect_idx = report_checker(report_copy)
+                if not incorrect_idx:
+                    safe_count += 1
+                    break
+    print(f"Part 2: result = {safe_count}")
 
 
 if __name__ == "__main__":

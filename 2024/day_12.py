027523f1513b69d0550a228f0ec200f54be3f376
Martin Harman <martinharman99@outlook.com>
2024-12-13 23:01:34 +0100

    day 12 part 2

--- 2024/day_12.py
+++ 2024/day_12.py
@@ -16,24 +16,72 @@ CHECKED = []
 REGIONS = defaultdict(dict)
 
 
-def part1():
+def solve():
     result = 0
     for y, line in enumerate(DATA):
         for x, char in enumerate(line):
             region = (x, y)
             REGIONS[region]["fences"] = []
-            REGIONS[region]["fields"] = 0
+            REGIONS[region]["fields"] = []
             check_neighbors(x, y, char, region)
-    for region in REGIONS.values():
-        result += len(region["fences"]) * region["fields"]
+    # Part 1
+    for region, values in REGIONS.items():
+        result += len(values["fences"]) * len(values["fields"])
     print(result)
+    result = 0
+    # Part 2
+    for region, values in REGIONS.items():
+        fences = 0
+        for x, y in values["fields"]:
+            corners = check_corner(x, y, region)
+            fences += corners
+        result += fences * len(values["fields"])
+    print(result)
+
+
+def check_corner(x, y, region):
+    corners_count = 0
+    up = (x, y - 1)
+    down = (x, y + 1)
+    left = (x - 1, y)
+    right = (x + 1, y)
+    up_right = (x + 1, y - 1)
+    up_left = (x - 1, y - 1)
+    down_right = (x + 1, y + 1)
+    down_left = (x - 1, y + 1)
+    fields = REGIONS[region]["fields"]
+    # !up && !left
+    if up not in fields and left not in fields:
+        corners_count += 1
+    # !down && !left
+    if down not in fields and left not in fields:
+        corners_count += 1
+    # !up && !right
+    if up not in fields and right not in fields:
+        corners_count += 1
+    # !down && !right
+    if down not in fields and right not in fields:
+        corners_count += 1
+    # up && left && !upleft
+    if up in fields and left in fields and up_left not in fields:
+        corners_count += 1
+    # down && left && !downleft
+    if down in fields and left in fields and down_left not in fields:
+        corners_count += 1
+    # up && right && !upright
+    if up in fields and right in fields and up_right not in fields:
+        corners_count += 1
+    # down && right && !downright
+    if down in fields and right in fields and down_right not in fields:
+        corners_count += 1
+    return corners_count
 
 
 def check_neighbors(x, y, char, region):
     if (x, y) in CHECKED:
         return
     CHECKED.append((x, y))
-    REGIONS[region]["fields"] += 1
+    REGIONS[region]["fields"].append((x, y))
     # fences = 0
     for dx, dy in DIRECTIONS:
         x1 = x + dx
@@ -51,7 +99,4 @@ def check_neighbors(x, y, char, region):
 
 if __name__ == "__main__":
     data = read_file()
-    part1()
-
-# 1402544
-# 871983
+    solve()

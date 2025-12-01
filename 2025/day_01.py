e82c9506c9730f77917efaddb1da8f5cd621d19e
Martin Harman <martinharman99@outlook.com>
2025-12-01 15:46:13 +0100

    day 1 part 1

---
+++ 2025/day_01.py
@@ -0,0 +1,42 @@
+import operator
+from pathlib import Path
+from typing import Literal
+
+
+def read_file():
+    with open(Path(__file__.replace(".py", ".txt"))) as f:
+        data = f.readlines()
+    return [(line[0], int(line[1:].rstrip("\n"))) for line in data]
+
+
+class Dial:
+    MIN_VALUE = 0
+    MAX_VALUE = 99
+    START_VALUE = 50
+
+    def __init__(self, start_value: int = START_VALUE):
+        self.current_value = start_value
+
+    def rotate(self, direction: Literal["L", "R"], value: int):
+        if direction not in ("L", "R"):
+            raise ValueError(
+                f"Unknown direction `{direction}`. Valid direction is `L` or `R`."
+            )
+        direction_op = operator.add if direction == "R" else operator.sub
+        new_current_value = direction_op(self.current_value, value) % (
+            self.MAX_VALUE + 1
+        )
+        if new_current_value < self.MIN_VALUE or new_current_value > self.MAX_VALUE:
+            raise ValueError(f"Invalid dial value: {new_current_value}")
+        self.current_value = new_current_value
+
+
+if __name__ == "__main__":
+    data = read_file()
+    dial = Dial()
+    final_pass = 0
+    for direction, value in data:
+        dial.rotate(direction, value)
+        if dial.current_value == 0:
+            final_pass += 1
+    print(final_pass)

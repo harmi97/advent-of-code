1f475d91524350a79b07baabd4e203b3ac568cf4
Martin Harman <martinharman99@outlook.com>
2025-12-02 11:27:12 +0100

    day 2 part 2

--- 2025/day_02.py
+++ 2025/day_02.py
@@ -18,9 +18,30 @@ def part_1(data):
                 num_2 = num[:half_len]
                 if num_1 == num_2:
                     repeated_n.append(int(num))
-    print(sum(repeated_n))
+    print(f"Part 1 = {sum(repeated_n)}")
+
+
+def part_2(data):
+    def _split_number(num_str: str, parts: int) -> bool:
+        num_parts = [num_str[i : i + parts] for i in range(0, len(num_str), parts)]
+        return len(set(num_parts)) == 1
+
+    repeated_n = []
+    for start_n, end_n in data:
+        for num in range(int(start_n), int(end_n) + 1):
+            num_str = str(num)
+            for iter_n in range(1, len(num_str) + 1):
+                # skip if can't be split into equal parts - min 2 parts
+                if len(num_str) % iter_n != 0 or iter_n == len(num_str):
+                    continue
+                is_repeating = _split_number(num_str, iter_n)
+                if is_repeating:
+                    repeated_n.append(num)
+                    break
+    print(f"Part 2 = {sum(repeated_n)}")
 
 
 if __name__ == "__main__":
     data = read_file()
     part_1(data)
+    part_2(data)

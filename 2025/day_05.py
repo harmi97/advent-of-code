4b5cac9bd79d49021c68e98e844cd2dd58e560c5
Martin Harman <martinharman99@outlook.com>
2025-12-05 14:03:51 +0100

    day 5 part 2

--- 2025/day_05.py
+++ 2025/day_05.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from pathlib import Path
 
 DATA = Path(__file__.replace(".py", ".txt")).read_text()
@@ -12,9 +13,40 @@ def part1(ranges: list[tuple[int, int]], items: list[int]):
     print(f"Part 1  = {fresh_items}")
 
 
+def part2(ranges: list[tuple[int, int]]):
+    ranges = set(ranges)
+    new_ranges = deepcopy(ranges)
+    while True:
+        updated = False
+        for rng1 in ranges:
+            for rng2 in ranges:
+                if rng2[0] <= rng1[0] <= rng2[1] and rng2 != rng1:
+                    flattened = [r2 for r1 in [rng1, rng2] for r2 in r1]
+                    min_val = min(flattened)
+                    max_val = max(flattened)
+                    # Range could already be removed by previous iteration
+                    try:
+                        new_ranges.remove(rng2)
+                    except KeyError:
+                        pass
+                    try:
+                        new_ranges.remove(rng1)
+                    except KeyError:
+                        pass
+                    new_ranges.add((min_val, max_val))
+                    updated = True
+                    break
+        ranges = deepcopy(new_ranges)
+        if not updated:
+            break
+    fresh_items = [(r[1] - r[0]) + 1 for r in ranges]
+    print(f"Part 2  = {sum(fresh_items)}")
+
+
 if __name__ == "__main__":
     data = DATA.split("\n\n")
     ranges = [(int(r.split("-")[0]), int(r.split("-")[1])) for r in data[0].split("\n")]
     items = [int(i) for i in data[1].split("\n")]
 
     part1(ranges, items)
+    part2(ranges)

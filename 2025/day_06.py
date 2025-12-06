fd151b81e899c48715d2e127c6e9dad56bceb0bb
Martin Harman <martinharman99@outlook.com>
2025-12-06 15:41:26 +0100

    day 6 part 2

--- 2025/day_06.py
+++ 2025/day_06.py
@@ -2,9 +2,11 @@ import math
 from pathlib import Path
 
 DATA = Path(__file__.replace(".py", ".txt")).read_text()
+MAX_DIGITS = 4
 
 
 def part1(data: list[list[str]]):
+    data = [[r1 for r1 in r0.strip(" ").split((" ")) if r1 != ""] for r0 in data]
     results = []
     max_cols = len(data[0])
     max_rows = len(data) - 1
@@ -19,7 +21,27 @@ def part1(data: list[list[str]]):
     print(f"Part 1  = {sum(results)}")
 
 
+def part2(data: list[list[str]]):
+    data = tuple(reversed(line) for line in data)
+    operator = None
+    numbers = []
+    results = []
+    for row in zip(*data):
+        try:
+            numbers.append(int("".join(row[:-1])))
+        except ValueError:  # New row
+            numbers = []
+            continue
+        operator = row[-1]
+        if operator == "+":
+            results.append(sum(numbers))
+        if operator == "*":
+            results.append(math.prod(numbers))
+
+    print(f"Part 2  = {sum(results)}")
+
+
 if __name__ == "__main__":
     data = DATA.splitlines()
-    data = [[r1 for r1 in r0.strip(" ").split((" ")) if r1 != ""] for r0 in data]
     part1(data)
+    part2(data)

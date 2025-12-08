0e62de28d54b846a7f2398515bd7a30e460c2970
Martin Harman <martinharman99@outlook.com>
2025-12-08 23:07:08 +0100

    refactor:  Split 2025-08  solution into separate parts

--- 2025/day_08.py
+++ 2025/day_08.py
@@ -4,17 +4,25 @@ from pathlib import Path
 
 from tqdm import tqdm
 
-DATA = Path(__file__.replace(".py", ".txt")).read_text()
+DATA = Path(__file__.replace(".py", ".txt")).read_text(encoding="utf-8")
 MAX_CONNECTIONS = 1000
 
 
-def part1(data: list[tuple(int, int, int)]):
-    part1_result = None
-    # Calculate distances
-    distances = {}
+def calculate_distances(input_data: list[tuple[int, int, int]]) -> dict[float, tuple]:
+    """Calculates distance between each junction box.
+
+    Args:
+        input_data (list[tuple[int, int, int]]):
+            List of tuples with X, Y Z coordinates of junction boxes.
+    Raises:
+        ValueError: Raised when identical distance is calculated.
+    Returns:
+        dict[float, tuple]: Sorted dictionary with distance as key and tuple with junction box pair.
+    """
     print("Calculating distances")
-    for jbox in tqdm(data):
-        other_jboxes = deepcopy(data)
+    distances = {}
+    for jbox in tqdm(input_data):
+        other_jboxes = deepcopy(input_data)
         other_jboxes.remove(jbox)
         for other_jbox in other_jboxes:
             distance = math.dist(jbox, other_jbox)
@@ -24,20 +32,34 @@ def part1(data: list[tuple(int, int, int)]):
             ):
                 raise ValueError
             distances[distance] = (jbox, other_jbox)
+    return dict(sorted(distances.items()))
 
-    # Construct circuits
+
+def make_circuits(
+    input_data: list[tuple[int, int, int]],
+    distances: dict[float, tuple],
+    max_connections: int | None = None,
+) -> list:
+    """Connects closest junction boxes with each other creating circuits.
+
+    Args:
+        input_data (list[tuple[int, int, int]]):
+            List of tuples with X, Y Z coordinates of junction boxes.
+        distances (dict[float, tuple]):
+            Sorted dictionary with distance as key and tuple with junction box pair.
+        max_connections (int | None, optional): Maximum allowed connections that can be made.
+            If set to None all junction boxes are connected into single circuit.
+            Defaults to None.
+    Returns:
+        list: List of new circuits with connected junction boxes.
+    """
     print("Making connections")
-    remaining_jboxes = deepcopy(data)
+    remaining_jboxes = deepcopy(input_data)
     new_circuits = []
-    distances = dict(sorted(distances.items()))
     connections = 0
     for jb1, jb2 in tqdm(distances.values()):
-        if connections == MAX_CONNECTIONS:
-            # if connections == 10:
-            new_circuits_lens = [len(x) for x in new_circuits]
-            new_circuits_lens.sort(reverse=True)
-            part1_result = math.prod(new_circuits_lens[:3])
-
+        if max_connections is not None and connections == max_connections:
+            break
         if jb1 not in remaining_jboxes and jb2 not in remaining_jboxes:
             jb1_circuit = None
             jb2_circuit = None
@@ -83,12 +105,31 @@ def part1(data: list[tuple(int, int, int)]):
             remaining_jboxes.remove(jb2)
             continue
         connections += 1
+    return new_circuits
+
+
+def part1(data: list[tuple(int, int, int)]):
+    print("Running Part 1")
+    distances = calculate_distances(data)
+    new_circuits = make_circuits(data, distances, MAX_CONNECTIONS)
+
+    new_circuits_lens = [len(x) for x in new_circuits]
+    new_circuits_lens.sort(reverse=True)
+    part1_result = math.prod(new_circuits_lens[:3])
+
+    print(f"Part 1  = {part1_result}")
+
+
+def part2(data: list[tuple(int, int, int)]):
+    print("Running Part 2")
+    distances = calculate_distances(data)
+    new_circuits = make_circuits(data, distances, None)
 
     last_value = new_circuits[0][-1]
     # Get closes box to last one except self
     closest_to_last = sorted(data, key=lambda x: math.dist(x, last_value))[1]
     part2_result = last_value[0] * closest_to_last[0]
-    print(f"Part 1  = {part1_result}")
+
     print(f"Part 2  = {part2_result}")
 
 
@@ -96,3 +137,4 @@ if __name__ == "__main__":
     data = DATA.splitlines()
     data = [tuple(int(x) for x in r.split(",")) for r in data]
     part1(data)
+    part2(data)
